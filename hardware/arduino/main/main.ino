9c6b384cfed2bf4b52695e7459b3839a6f17a5ef
Roland Halász <roland.halasz@outlook.com>
2022-05-22 16:08:50 +0200

    reading temperature and humidity

--- hardware/arduino/main/main.ino
+++ hardware/arduino/main/main.ino
@@ -2,12 +2,39 @@
  *  DHT22: https://create.arduino.cc/projecthub/mafzal/temperature-monitoring-with-dht22-arduino-15b013 
 */
 
-void setup() {
-  // put your setup code here, to run once:
+//Libraries
+#include <DHT.h>;
 
+//Constants
+#define DHTPIN 7  // what pin we're connected to
+#define DHTTYPE DHT22 // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor for normal 16mhz Arduino
+
+//Variables
+float hum;  //Stores humidity value
+float temp; //Stores temperature value
+
+// Put your setup code here, to run once
+void setup() 
+{
+  Serial.begin(9600);
+  dht.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+// put your main code here, to run repeatedly:
+void loop() 
+{
+  // 2 sec. delay required my dht22
+  delay(2500);
+  
+  //Read data and store it to variables hum and temp
+  hum = dht.readHumidity();
+  temp= dht.readTemperature();
+  
+  //Print temp and humidity values to serial monitor
+  Serial.print("Humidity: ");
+  Serial.print(hum);
+  Serial.print(" %, Temp: ");
+  Serial.print(temp);
+  Serial.println(" Celsius");
 }
