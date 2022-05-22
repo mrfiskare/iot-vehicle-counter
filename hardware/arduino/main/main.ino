c152d19c1e977285d62e3a7dc951cf84251141d0
Roland Halász <roland.halasz@outlook.com>
2022-05-22 13:25:38 +0200

    new sketch created

---
+++ hardware/arduino/main/main.ino
@@ -0,0 +1,13 @@
+/* 
+ *  Ref: https://create.arduino.cc/projecthub/mafzal/temperature-monitoring-with-dht22-arduino-15b013 
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
