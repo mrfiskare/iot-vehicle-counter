86cea6b3e0834a08e703a28fc48c335d3d85bf77
Roland Halász <roland.halasz@outlook.com>
2023-02-12 20:39:44 +0100

    feat(#3): add file path variable

--- cam-record.sh
+++ cam-record.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 # Stop running in case of failure
+
 set -e
 
 # Get the current date and time
+
 date=$(date +%F_%H-%M)
 
 # Set directory for video files
-cam_output="/home/pi/cam_output"
+
+cam_output="/home/pi/cam_output/"
 mkdir -p "$cam_output"
 
+# Set the name of the video files
+
+file_name="recording.h264"
+file_path="$cam_output""$file_name"
+
 # Record raw video
+
 raspivid \
     --width 640 \
     --height 640 \
@@ -18,4 +27,4 @@ raspivid \
     --rotation 180 \
     --framerate 15 \
     --roi 0.3,0.3,0.6,0.6 \
-    --output "$cam_output"".h264"
+    --output "$file_path"
