b5f904fc9a4ec2ddc9c32bfc09a1d03d68440d5e
Roland Halász <roland.halasz@outlook.com>
2023-02-22 17:07:31 +0100

    chore(#3): try to fix framerate issue

--- record-video-python.py
+++ record-video-python.py
@@ -8,7 +8,7 @@ camera.resolution = (640, 640)
 camera.vflip = True
 camera.hflip = True
 camera.zoom=(0.3,0.3,0.7,0.7)
-camera.framerate = 15
+# camera.framerate = 15
 
 # Set the recording length (in seconds)
 
