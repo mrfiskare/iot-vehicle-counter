4f60793ad24de3f1d97cc849d2b9c688bfc7348f
Roland Halász <roland.halasz@outlook.com>
2023-02-22 16:18:42 +0100

    feat(#3): define variables

--- record-video-python.py
+++ record-video-python.py
@@ -1,6 +1,8 @@
 from picamera import PiCamera
 from time import sleep
 
+# Set up the PiCamera object
+
 camera = PiCamera()
 camera.resolution = (640, 640)
 camera.vflip = True
@@ -8,8 +10,22 @@ camera.hflip = True
 camera.zoom=(0.3,0.3,0.7,0.7)
 camera.framerate = 15
 
+# Set the recording length (in seconds)
+
+recording_length = 60
+
+# Set the total recording time (in seconds)
+
+total_recording_time = 8 * 60
+
+# Set the output directory
+
+output_directory = "/home/pi/"
+
 # Record 4 1-minute long videos
-file_name = '/home/pi/video_{:02d}.h264'
+
+file_name = output_directory + 'video_{:02d}.h264'
+
 for i in range(4):
     camera.start_recording(file_name.format(i))
     camera.wait_recording(60)  # 1 minute = 60 seconds
