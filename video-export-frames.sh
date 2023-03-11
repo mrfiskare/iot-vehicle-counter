e1f090423663a90eac60988221bc715ddb29cfe9
Roland Halász <roland.halasz@outlook.com>
2023-03-11 17:02:58 +0100

    feat(#3): print elapsed time

--- video-export-frames.sh
+++ video-export-frames.sh
@@ -4,6 +4,10 @@
 
 set -e
 
+# Reset BASH time counter
+
+SECONDS=0
+
 # Export every 20th frame (every 2nd sec) from the video
 
 for file in /home/pi/recording/recorded/*.h264
@@ -11,3 +15,5 @@ do
     ffmpeg -i "$file" -vf "select=not(mod(n\,20))" -vsync vfr "${file%.*}"_%05d.jpg
     rm -rf "$file"
 done
+
+ELAPSED="Elapsed: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
