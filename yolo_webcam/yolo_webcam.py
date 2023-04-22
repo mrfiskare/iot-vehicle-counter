56dcf4e64c71a0700576e689b4526115df1d335f
Roland Halász <roland.halasz@outlook.com>
2023-04-22 21:33:16 +0200

    feat: draw bounding boxes around objects

---
+++ yolo_webcam/yolo_webcam.py
@@ -0,0 +1,26 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+model = YOLO("../yolo_weights/yolov8n.pt")
+
+while True:
+    success, img = cap.read()
+    results = model(img, stream=True)
+
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            # cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            w, h = x2-x1, y2-y1
+            bbox = x1, y1, w, h
+            cvzone.cornerRect(img, bbox)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
