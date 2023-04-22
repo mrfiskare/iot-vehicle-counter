6b4d15dec929e1a8071e137881d19ef2e517809c
Roland Halász <roland.halasz@outlook.com>
2023-04-22 21:09:36 +0200

    feat: test yolo with webcam

--- running_yolo/yolo_basics.py
+++ running_yolo/yolo_basics.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 import cv2
 
 model = YOLO('../yolo_weights/yolov8n.pt')
-results = model("images/4.jpg", show=True)
+results = model("../images/4.jpg", show=True)
 
 cv2.waitKey(0)
