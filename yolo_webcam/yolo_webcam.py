d738f2c627ff4754792ac022b64846348704aa7c
Roland Halász <roland.halasz@outlook.com>
2023-04-22 22:10:07 +0200

    feat: define coco-classes

--- yolo_webcam/yolo_webcam.py
+++ yolo_webcam/yolo_webcam.py
@@ -3,11 +3,27 @@ import cv2
 import cvzone
 import math
 
+# Initialize webcam
+
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
+# Initialize YOLO
+
 model = YOLO("../yolo_weights/yolov8n.pt")
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus",
+              "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign",
+              "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
+              "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag",
+              "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite",
+              "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket",
+              "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana",
+              "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza",
+              "donut", "cake", "chair", "sofa", "pottedplant", "bed", "diningtable",
+              "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock",
+              "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
 
 while True:
     success, img = cap.read()
