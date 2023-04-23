6b171887e557be2a1a62357062784700b4a9cf99
Roland Halász <roland.halasz@outlook.com>
2023-04-23 18:05:08 +0200

    fix: show the tracked objects ids properly

--- main.py
+++ main.py
@@ -26,9 +26,16 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus",
               "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
 vehicleTypes = ["car", "motorbike", "bus", "truck"]
 
-# Tracking using sort by abewley
+# Initializing Sort tracker made by abewley
 
-tracker = Sort(max_age=10, min_hits=3, iou_threshold=0.3)
+tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+
+
+def calculate_w_h(x_1, y_1, x_2, y_2):
+    width = x_2 - x_1
+    height = y_2 - y_1
+    return width, height
+
 
 while True:
     success, img = cap.read()
@@ -43,7 +50,7 @@ while True:
         for box in boxes:
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            w, h = x2 - x1, y2 - y1
+            w, h = calculate_w_h(x1, y1, x2, y2)
             bbox = x1, y1, w, h
 
             # Rounding confidence to 2 decimals
@@ -62,25 +69,36 @@ while True:
 
                 if currentClass in vehicleTypes:
 
-                    # Printing information
+                    # Showing detection bounding box, conf. and class name (commented out)
 
-                    print(f'class: {currentClass} {conf}')
-                    cvzone.cornerRect(img, bbox, l=8, t=2)
-                    cvzone.putTextRect(img, f'{currentClass} {conf}',
-                                       (max(0, x1), max(30, y1)),
-                                       scale=0.8, thickness=1, offset=2)
+                    cvzone.cornerRect(img, bbox, l=8, t=2, rt=1)
+                    # cvzone.putTextRect(img, f'{currentClass} {conf}',
+                    #                    (max(0, x1), max(30, y1)),
+                    #                    scale=0.8, thickness=1, offset=2)
+                    # print(f'class: {currentClass} {conf}')
 
                     # Adding the detected objects to the detections array
 
-                    currentArray = np.array([x1,y1,x2,y2,conf])
+                    currentArray = np.array([x1, y1, x2, y2, conf])
                     detections = np.vstack((detections, currentArray))
 
     # Assigning tracking IDs to the detected objects
 
     resultsTracker = tracker.update(detections)
     for results in resultsTracker:
-        x1, y1, x2, y2, id = results
-        print(results)
 
-    cv2.imshow("Image", img)
+        # Getting rect. data from the result
+
+        t_x1, t_y1, t_x2, t_y2, tracking_id = results
+        t_x1, t_y1, t_x2, t_y2, tracking_id = int(t_x1), int(t_y1), int(t_x2), int(t_y2), int(tracking_id)
+        t_w, t_h = calculate_w_h(t_x1, t_y1, t_x2, t_y2)
+
+        # Showing tracking bounding box, tracked object's id
+
+        cvzone.cornerRect(img, (t_x1, t_y1, t_w, t_h), l=8, t=2, rt=1, colorR=(255, 0, 0))
+        cvzone.putTextRect(img, f'{tracking_id}',
+                           (max(0, t_x1), max(30, t_y1)),
+                           scale=0.8, thickness=1, offset=2)
+
+    cv2.imshow("Object counter", img)
     cv2.waitKey(1)
