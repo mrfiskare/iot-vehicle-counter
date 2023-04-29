22844af317fae21c8e020f77b1d3cdecd97e64e4
Roland Halász <roland.halasz@outlook.com>
2023-04-29 18:00:21 +0200

    fix: finalize usage with ids and types

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sort import *
 
 # Initialize webcam
 
-cap = cv2.VideoCapture("videos/motors2.h264")
+cap = cv2.VideoCapture("videos/traffic_jam2.h264")
 
 # Initialize YOLO
 
@@ -24,6 +24,7 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus",
               "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
               "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock",
               "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+vehicleTypes = ["car", "motorbike", "bus", "truck"]
 
 # Initializing Sort tracker made by abewley
 
@@ -49,26 +50,74 @@ def calculate_w_h(x_1, y_1, x_2, y_2):
     return width, height
 
 
-def update_tracker(tracker, detections, count, type):
-    tracker_results = tracker.update(detections)
+def sort_array(np_array):
+    sorted_np_array = np.argsort(np_array[:, 0])
+    return np_array[sorted_np_array]
 
-    print(f'update tracker with {type}')
+
+def match_array_tuples(detections_arr, tracking_arr):
+    # The resulting array with [x1, y1, x2, y2, id, type]
+
+    arr_with_id_and_type = np.empty((0, 6))
+
+    # Removing unnecessary values for matching
+
+    detections_arr_coordinates_only = detections_arr[:, :-2]
+    tracking_arr_coordinates_only = tracking_arr[:, :-1]
+
+    for index, arr_tuple in enumerate(tracking_arr_coordinates_only):
+
+        # Compute the Euclidean distances
+
+        distances = np.linalg.norm(detections_arr_coordinates_only - arr_tuple, axis=1)
+
+        # Find the index of the minimum distance
+
+        min_distance_index = np.argmin(distances)
+
+        # Get the closest row
+        # closest_row = detections_arr[min_distance_index]
+
+        # Generate the resulting row with coordinates, tracking id and vehicle type id
+
+        result = np.array([
+            [tracking_arr[index, 0],
+             tracking_arr[index, 1],
+             tracking_arr[index, 2],
+             tracking_arr[index, 3],
+             tracking_arr[index, 4],
+             detections_arr[min_distance_index, 5]]
+        ])
+
+        arr_with_id_and_type = np.vstack([arr_with_id_and_type, result])
+
+    return arr_with_id_and_type.astype(int)
+
+
+def update_tracker(tracker, detections, count):
+
+    # Matching the rows from detections to tracker_results.
+    # This is needed because the detections array contains the vehicle types,
+    # whereas the tracker_results array contains the tracking ids.
+
+    tracker_results = tracker.update(detections)
+    tracker_results = match_array_tuples(detections, tracker_results)
 
     for tracked_object in tracker_results:
 
-        # Getting rect. data from the result
+        # Getting coordinates, id, type data from tracker_results
 
-        t_x1, t_y1, t_x2, t_y2, tracking_id = tracked_object
-        t_x1, t_y1, t_x2, t_y2, tracking_id = int(t_x1), int(t_y1), int(t_x2), int(t_y2), int(tracking_id)
+        t_x1, t_y1, t_x2, t_y2, tracking_id, type_index = tracked_object
         t_w, t_h = calculate_w_h(t_x1, t_y1, t_x2, t_y2)
+        cls_name = classNames[type_index]
+        print(f'[{tracking_id}] {cls_name}')
 
         # Showing tracking bounding box, tracked object's id
 
         cvzone.cornerRect(img, (t_x1, t_y1, t_w, t_h), l=8, t=2, rt=1, colorR=(255, 0, 0))
-        cvzone.putTextRect(img, f'[{tracking_id}]',
+        cvzone.putTextRect(img, f'[{tracking_id}] {cls_name}',
                            (max(0, t_x1), max(30, t_y1)),
                            scale=0.8, thickness=1, offset=2, colorR=(0, 0, 102))
-        print(f'tracking rect for {type}')
 
         # Center points
 
@@ -88,10 +137,7 @@ while True:
     success, img = cap.read()
     results = model(img, stream=True)
 
-    carDetections = np.empty((0, 5))
-    motorbikeDetections = np.empty((0, 5))
-    busDetections = np.empty((0, 5))
-    truckDetections = np.empty((0, 5))
+    yoloDetections = np.empty((0, 6))
 
     for r in results:
 
@@ -115,30 +161,24 @@ while True:
 
             # Defining the parameter for the sorting algorithm
 
-            currentArray = np.array([x1, y1, x2, y2, conf])
+            currentArray = np.array([x1, y1, x2, y2, conf, cls])
 
             # If the conf. level is high enough, printing the conf. and class name
             # to the terminal and bounding box for selected classes
 
             if conf > 0.3:
 
-                cvzone.cornerRect(img, bbox, l=8, t=2, rt=1)
-                cvzone.putTextRect(img, f'{currentClass}',
-                                   (max(0, x1), max(30, y1)),
-                                   scale=0.8, thickness=1, offset=2)
+                # cvzone.cornerRect(img, bbox, l=8, t=2, rt=1)
+                # cvzone.putTextRect(img, f'{currentClass}',
+                #                    (max(0, x1), max(30, y1)),
+                #                    scale=0.8, thickness=1, offset=2)
 
                 if currentClass == "car":
-                    carDetections = np.vstack((carDetections, currentArray))
-                    print("current class: car")
-
-
+                    yoloDetections = np.vstack((yoloDetections, currentArray))
 
     # Assigning tracking IDs to the detected objects
 
-    carCount = update_tracker(carTracker, carDetections, carCount, "car")
-
-
-
+    carCount = update_tracker(carTracker, yoloDetections, carCount)
 
     # Drawing the lines
 
@@ -151,9 +191,10 @@ while True:
     # Displaying counters
 
     cvzone.putTextRect(img, f'car: {len(carCount)}', (20, 400), scale=1.2, thickness=1, offset=2, colorR=(0, 0, 0))
-    cvzone.putTextRect(img, f'motorbike: {len(motorbikeCount)}', (20, 420), scale=1.2, thickness=1, offset=2, colorR=(0, 0, 0))
+    cvzone.putTextRect(img, f'motorbike: {len(motorbikeCount)}', (20, 420), scale=1.2, thickness=1, offset=2,
+                       colorR=(0, 0, 0))
     cvzone.putTextRect(img, f'bus: {len(busCount)}', (20, 440), scale=1.2, thickness=1, offset=2, colorR=(0, 0, 0))
     cvzone.putTextRect(img, f'truck: {len(truckCount)}', (20, 460), scale=1.2, thickness=1, offset=2, colorR=(0, 0, 0))
 
-    cv2.imshow("Object counter", img)
-    cv2.waitKey(0)
+    cv2.imshow("Vehicle counter", img)
+    cv2.waitKey(1)
