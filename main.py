92201d2d0e75c928932adbe59e9001be52086d03
Roland Halász <roland.halasz@outlook.com>
2023-05-02 00:56:28 +0200

    feat: add automatization and json creation

---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+import json
+import glob
+import time
+import datetime
+from pytz import timezone
+from vehicle_counter import *
+
+def count_vehicles(file_path):
+    # Your implementation for counting vehicles goes here
+    # Example implementation
+    carCount = 5
+    motorbikeCount = 3
+    busCount = 2
+    truckCount = 1
+
+    return carCount, motorbikeCount, busCount, truckCount
+
+def convert_to_iso(timestamp_str):
+    timestamp = datetime.datetime.strptime(timestamp_str, "%Y-%m-%d_%H-%M")
+    timestamp = timestamp.replace(tzinfo=timezone('Europe/Budapest'))
+    return timestamp.isoformat()
+
+output_folder = "C:/videos/output"
+json_folder = "C:/videos/unprocessed_json"
+json_file = "measurements.json"
+
+if not os.path.exists(json_folder):
+    os.makedirs(json_folder)
+
+json_path = os.path.join(json_folder, json_file)
+
+data = []
+
+if os.path.exists(json_path):
+    with open(json_path, 'r') as file:
+        data = json.load(file)
+
+vehicle_counter = VehicleCounter()
+
+for file_path in glob.glob(os.path.join(output_folder, '*.mkv')):
+    filename = os.path.basename(file_path)
+    timestamp_str = os.path.splitext(filename)[0]
+    timestamp_iso = convert_to_iso(timestamp_str)
+
+    carCount, motorbikeCount, busCount, truckCount = vehicle_counter.count_vehicles(file_path)
+
+    data.append({
+        'timestamp': timestamp_iso,
+        'carCount': carCount,
+        'motorbikeCount': motorbikeCount,
+        'busCount': busCount,
+        'truckCount': truckCount
+    })
+
+with open(json_path, 'w') as file:
+    json.dump(data, file, indent=4)
--- vehicle_counter.py
+++ vehicle_counter.py
@@ -5,56 +5,6 @@ import cvzone
 import math
 from sort import *
 
-# Initialize webcam
-
-cap = cv2.VideoCapture("videos/various.mp4")
-showImg = True
-
-# Initialize YOLO
-
-model = YOLO("yolo_weights/yolov8n.pt")
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus",
-              "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign",
-              "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
-              "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag",
-              "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite",
-              "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket",
-              "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana",
-              "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza",
-              "donut", "cake", "chair", "sofa", "pottedplant", "bed", "diningtable",
-              "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock",
-              "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
-vehicleTypes = ["car", "motorbike", "bus", "truck"]
-
-# Initializing Sort tracker made by abewley
-
-maxAge = 20
-minHits = 3
-iouThreshold = 0.3
-
-vehicleTracker = Sort(max_age=maxAge, min_hits=minHits, iou_threshold=iouThreshold)
-
-# Line positions
-
-linePosition = [400, 0, 400, 480]
-offset = 30
-carCount = []
-motorbikeCount = []
-busCount = []
-truckCount = []
-
-
-def calculate_w_h(x_1, y_1, x_2, y_2):
-    width = x_2 - x_1
-    height = y_2 - y_1
-    return width, height
-
-
-def sort_array(np_array):
-    sorted_np_array = np.argsort(np_array[:, 0])
-    return np_array[sorted_np_array]
-
 
 def match_array_tuples(detections_arr, tracking_arr):
     # The resulting array with [x1, y1, x2, y2, id, type]
@@ -67,7 +17,6 @@ def match_array_tuples(detections_arr, tracking_arr):
     tracking_arr_coordinates_only = tracking_arr[:, :-1]
 
     for index, arr_tuple in enumerate(tracking_arr_coordinates_only):
-
         # Compute the Euclidean distances
 
         distances = np.linalg.norm(detections_arr_coordinates_only - arr_tuple, axis=1)
@@ -95,125 +44,142 @@ def match_array_tuples(detections_arr, tracking_arr):
     return arr_with_id_and_type.astype(int)
 
 
-def update_tracker(tracker, detections):
-
-    # Matching the rows from detections to tracker_results.
-    # This is needed because the detections array contains the vehicle types,
-    # whereas the tracker_results array contains the tracking ids.
-
-    tracker_results = tracker.update(detections)
-    tracker_results = match_array_tuples(detections, tracker_results)
-
-    for tracked_object in tracker_results:
-
-        # Getting coordinates, id, type data from tracker_results
-
-        t_x1, t_y1, t_x2, t_y2, tracking_id, type_index = tracked_object
-        t_w, t_h = calculate_w_h(t_x1, t_y1, t_x2, t_y2)
-        cls_name = classNames[type_index]
-
-        # Showing tracking bounding box, tracked object's id
-
-        if showImg:
-            cvzone.cornerRect(img, (t_x1, t_y1, t_w, t_h), l=8, t=2, rt=1, colorR=(255, 0, 0))
-            cvzone.putTextRect(img, f'[{tracking_id}] {cls_name}',
-                               (max(0, t_x1), max(30, t_y1)),
-                               scale=0.8, thickness=1, offset=2, colorR=(0, 0, 102))
-
-        # Center points
-
-        t_cx, t_cy = t_x1 + t_w // 2, t_y1 + t_h // 2
-        cv2.circle(img, (t_cx, t_cy), 3, (255, 0, 255), cv2.FILLED)
-
-        # Increase the counter if the id has not been counted before
-
-        if linePosition[0] - offset < t_cx < linePosition[0] + offset:
-
-            if cls_name == "car" and carCount.count(tracking_id) == 0:
-                carCount.append(tracking_id)
-
-            if cls_name == "motorbike" and motorbikeCount.count(tracking_id) == 0:
-                motorbikeCount.append(tracking_id)
-
-            if cls_name == "truck" and truckCount.count(tracking_id) == 0:
-                truckCount.append(tracking_id)
-
-            if cls_name == "bus" and busCount.count(tracking_id) == 0:
-                busCount.append(tracking_id)
+def calculate_w_h(x_1, y_1, x_2, y_2):
+    width = x_2 - x_1
+    height = y_2 - y_1
+    return width, height
 
 
-while True:
-    success, img = cap.read()
-    results = model(img, stream=True)
+def sort_array(np_array):
+    sorted_np_array = np.argsort(np_array[:, 0])
+    return np_array[sorted_np_array]
 
-    yoloDetections = np.empty((0, 6))
 
-    for r in results:
+class VehicleCounter:
 
-        # Drawing bounding boxes around specified objects with cvzone
+    # Initialize YOLO
 
-        boxes = r.boxes
-        for box in boxes:
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            w, h = calculate_w_h(x1, y1, x2, y2)
-            bbox = x1, y1, w, h
+    model = YOLO("yolo_weights/yolov8n.pt")
+    classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus",
+                  "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign",
+                  "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
+                  "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag",
+                  "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite",
+                  "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket",
+                  "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana",
+                  "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza",
+                  "donut", "cake", "chair", "sofa", "pottedplant", "bed", "diningtable",
+                  "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+                  "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock",
+                  "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+    vehicleTypes = ["car", "motorbike", "bus", "truck"]
 
-            # Rounding confidence to 2 decimals
+    # Initializing Sort tracker made by abewley
 
-            conf = math.ceil(box.conf[0] * 100) / 100
+    maxAge = 20
+    minHits = 3
+    iouThreshold = 0.3
 
-            # Getting the class name, using the pre-defined array of coco-classes
+    vehicleTracker = Sort(max_age=maxAge, min_hits=minHits, iou_threshold=iouThreshold)
 
-            cls = int(box.cls[0])
-            currentClass = classNames[cls]
+    # Line positions
 
-            # Defining the parameter for the sorting algorithm
+    linePosition = [400, 0, 400, 480]
+    offset = 30
+    carCount = []
+    motorbikeCount = []
+    busCount = []
+    truckCount = []
 
-            currentArray = np.array([x1, y1, x2, y2, conf, cls])
+    def update_tracker(self, tracker, detections, img):
 
-            # If the conf. level is high enough, printing the conf. and class name
-            # to the terminal and bounding box for selected classes
+        # Matching the rows from detections to tracker_results.
+        # This is needed because the detections array contains the vehicle types,
+        # whereas the tracker_results array contains the tracking ids.
 
-            if conf > 0.2:
+        tracker_results = tracker.update(detections)
+        tracker_results = match_array_tuples(detections, tracker_results)
 
-                # if showImg:
-                #     cvzone.cornerRect(img, bbox, l=8, t=2, rt=1)
-                #     cvzone.putTextRect(img, f'{currentClass}',
-                #                        (max(0, x1), max(30, y1)),
-                #                        scale=0.8, thickness=1, offset=2)
+        for tracked_object in tracker_results:
 
-                if currentClass in vehicleTypes:
-                    yoloDetections = np.vstack((yoloDetections, currentArray))
+            # Getting coordinates, id, type data from tracker_results
 
-    # Assigning tracking IDs to the detected objects
+            t_x1, t_y1, t_x2, t_y2, tracking_id, type_index = tracked_object
+            t_w, t_h = calculate_w_h(t_x1, t_y1, t_x2, t_y2)
+            cls_name = self.classNames[type_index]
 
-    update_tracker(vehicleTracker, yoloDetections)
+            # Showing tracking bounding box, tracked object's id
 
-    # Drawing the lines
+            # Center points
 
-    if showImg:
-        cv2.line(img, (linePosition[0], linePosition[1]), (linePosition[2], linePosition[3]), (0, 0, 255), 2)
-        cv2.line(img, (linePosition[0] - offset, linePosition[1]), (linePosition[2] - offset, linePosition[3]),
-                 (153, 153, 255), 1)
-        cv2.line(img, (linePosition[0] + offset, linePosition[1]), (linePosition[2] + offset, linePosition[3]),
-                 (153, 153, 255), 1)
+            t_cx, t_cy = t_x1 + t_w // 2, t_y1 + t_h // 2
+            cv2.circle(img, (t_cx, t_cy), 3, (255, 0, 255), cv2.FILLED)
 
-    # Displaying counters
+            # Increase the counter if the id has not been counted before
 
-    if showImg:
-        cvzone.putTextRect(img, f'{"car:":<13}{len(carCount)}', (20, 400), scale=1.2, thickness=1, offset=2, colorR=(0, 0, 0))
-        cvzone.putTextRect(img, f'{"motorbike:":<12}{len(motorbikeCount)}', (20, 420), scale=1.2, thickness=1, offset=2,
-                           colorR=(0, 0, 0))
-        cvzone.putTextRect(img, f'{"truck:":<13}{len(truckCount)}', (20, 440), scale=1.2, thickness=1, offset=2, colorR=(0, 0, 0))
-        cvzone.putTextRect(img, f'{"bus:":<13}{len(busCount)}', (20, 460), scale=1.2, thickness=1, offset=2,
-                           colorR=(0, 0, 0))
+            if self.linePosition[0] - self.offset < t_cx < self.linePosition[0] + self.offset:
 
-    print(f'{"car:":<12}{len(carCount)}')
-    print(f'{"motorbike:":<12}{len(motorbikeCount)}')
-    print(f'{"truck:":<12}{len(truckCount)}')
-    print(f'{"bus:":<12}{len(busCount)}')
+                if cls_name == "car" and self.carCount.count(tracking_id) == 0:
+                    self.carCount.append(tracking_id)
 
-    if showImg:
-        cv2.imshow("Vehicle counter", img)
-    cv2.waitKey(1)
+                if cls_name == "motorbike" and self.motorbikeCount.count(tracking_id) == 0:
+                    self.motorbikeCount.append(tracking_id)
+
+                if cls_name == "truck" and self.truckCount.count(tracking_id) == 0:
+                    self.truckCount.append(tracking_id)
+
+                if cls_name == "bus" and self.busCount.count(tracking_id) == 0:
+                    self.busCount.append(tracking_id)
+
+        def count(file_path):
+
+            # Initialize video
+
+            cap = cv2.VideoCapture(file_path)
+
+            while True:
+                success, img = cap.read()
+                results = self.model(img, stream=True)
+                yolo_detections = np.empty((0, 6))
+
+                for r in results:
+
+                    # Drawing bounding boxes around specified objects with cvzone
+
+                    boxes = r.boxes
+                    for box in boxes:
+                        x1, y1, x2, y2 = box.xyxy[0]
+                        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                        w, h = calculate_w_h(x1, y1, x2, y2)
+                        bbox = x1, y1, w, h
+
+                        # Rounding confidence to 2 decimals
+
+                        conf = math.ceil(box.conf[0] * 100) / 100
+
+                        # Getting the class name, using the pre-defined array of coco-classes
+
+                        cls = int(box.cls[0])
+                        current_class = self.classNames[cls]
+
+                        # Defining the parameter for the sorting algorithm
+
+                        current_array = np.array([x1, y1, x2, y2, conf, cls])
+
+                        # If the conf. level is high enough, printing the conf. and class name
+                        # to the terminal and bounding box for selected classes
+
+                        if conf > 0.2:
+
+                            if current_class in self.vehicleTypes:
+                                yolo_detections = np.vstack((yolo_detections, current_array))
+
+                # Assigning tracking IDs to the detected objects
+
+                self.update_tracker(self.vehicleTracker, yolo_detections, img)
+                cv2.waitKey(0)
+
+        return len(self.carCount), len(self.motorbikeCount), len(self.busCount), len(self.truckCount)
+
+
+
