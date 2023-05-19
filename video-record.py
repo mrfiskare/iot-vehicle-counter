9a930ffbde0a16a7892ffce9a1f20fe7b5d5e63e
Roland Halász <roland.halasz@outlook.com>
2023-05-19 16:40:12 +0200

    feat: add upload script

--- video-record.py
+++ video-record.py
@@ -43,7 +43,7 @@ recording_length = 60 * 60
 # Set the output directories
 
 output_directory = "/home/pi/recording/"
-done_directory = "/home/pi/windows-share/"
+done_directory = "/home/pi/recording/recorded/"
 
 # Delete previous recordings
 
